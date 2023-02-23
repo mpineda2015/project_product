bc2639678b737a21dbba76d1f591d638085dc918
mpineda2015 <miguelpineda434@gmail.com>
2023-02-23 15:36:03 +0000

    Update metropolis.py
    adding copy Copyright

--- metropolis.py
+++ metropolis.py
@@ -1,3 +1,7 @@
+#Copyright
+
+#All of the content (text and images) on this website, with the exception of that provided by third parties, is copyright (C) Christopher Woods, 2006-2015.
+
 import random
 import math
 import copy
